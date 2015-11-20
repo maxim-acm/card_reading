003567a8c04a7d28d52c2ce8b696ee1a53babd4f
Max Kochanov <maxim-acm@yandex.ru>
2015-11-20 15:23:05 +0200

    changed result grid, cards hover(jquery), added settimeout to the result boxes

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
  * Created by Maxim on 05.11.2015.
  */
 
-$( document ).ready( function (){
+$( window ).load( function (){
 
     function Game() {
 
@@ -13,16 +13,18 @@ $( document ).ready( function (){
                 restartButton:      $( '#restart-divination' )
             },
             __self = this,
-            currentCardsSel = -1,
+            currentCardsSel = 0,
             shuffleCards = [ ];
 
         this.init = function () {
+
             __self.showCards();
             __self.selectCard();
 
         };
 
         this.showCards = function () {
+
             domElem.cards.each( function ( elem ) {
                 ( function ( that, i ) {
                     var t = setTimeout( function(){
@@ -58,38 +60,36 @@ $( document ).ready( function (){
             shuffleCards = cardsData.item.slice();
             shuffle( shuffleCards );
 
-            console.log(shuffleCards);
-
             domElem.cards.on( 'click', function (){
 
-                if ( currentCardsSel < 4 ) {
-                //    console.log(currentCardsSel);
-                    console.log( this );
+                if ( currentCardsSel < 5 ) {
+
                     $( this ).addClass( 'card-selected' );
 
-                    var currentCardNumber = $( this ).data( 'number' );
-                //    console.log(currentCardNumber);
+                    var domCardNumber = $( this ).data( 'number' );
+
                     currentCardsSel++;
-                    __self.addCard( currentCardNumber );
+                    __self.addCard( domCardNumber );
 
                 }
-
-
             })
         };
 
         this.addCard = function ( cardNumber ) {
 
-            var card = document.getElementById( 'choosen-cards' ).children[ currentCardsSel ];
-            console.log(card);
+            var card = document.getElementById( 'choosen-cards' ).children[ currentCardsSel - 1 ];
 
-            if (shuffleCards[ cardNumber - 1 ][ 'card' ] <= 41) {
-                $(card).addClass('box-wide');
+            var shuffleCardNumber = shuffleCards[ cardNumber - 1 ][ 'card' ];
+
+            if (shuffleCardNumber <= 41) {
+                $(card).addClass( 'box-wide' );
             }
-            card.querySelector( '.l-card-md' ).setAttribute( 'src', 'cards-images/Cards_v-' + shuffleCards[ cardNumber - 1 ][ 'card' ] + '.jpg' );
-            card.querySelector( '.l-description' ).innerHTML = shuffleCards[ cardNumber - 1 ][ 'description' ];
+            card.querySelector( '.l-card-md' ).setAttribute( 'src', 'cards-images/Cards_v-' + shuffleCardNumber + '.jpg' );
+            //card.querySelector( '.l-description' ).innerHTML = shuffleCards[ cardNumber - 1 ][ 'description' ];
 
-            $(card).removeClass( 'hidden' );
+            setTimeout(function(){
+                $(card).removeClass( 'hidden' );
+            }, 100);
 
         };
 
@@ -97,25 +97,21 @@ $( document ).ready( function (){
             domElem.cards.each( function () {
                 $( this ).addClass( 'hidden' );
                 $( this ).removeClass( 'card-selected' );
-            });
 
-            domElem.cardBox.each( function () {
-
-                this.hidden = true;
             });
 
             domElem.resultContainer.children().each( function() {
-               $( this).addClass('hidden');
+                $( this ).addClass( 'hidden' );
+                $( this ).removeClass( 'box-wide' );
             });
 
             domElem.cards.off( 'click' );
             domElem.restartButton.off( 'click' );
 
+            currentCardsSel = 0;
 
-            currentCardsSel = -1;
             game.init();
         }
-
     }
 
     window.game = new Game();
